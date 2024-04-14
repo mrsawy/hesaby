4547ec7d1e12a23eb3933517aa952ccbce2b999d
mrsawy <mrsawy@gmail.com>
2024-04-15 01:56:35 +0200

    small edit in middlewarer

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+// import { jwtVerify } from "jose";
 
-import { jwtVerify } from "jose";
-const JWT_SECRET_ADMIN: string = process.env.JWT_SECRET_ADMIN as string;
+// const JWT_SECRET_ADMIN: string = process.env.JWT_SECRET_ADMIN as string;
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
@@ -12,17 +12,17 @@ export async function middleware(request: NextRequest) {
   if (!request.cookies.has("admin-token")) {
     return NextResponse.redirect(new URL("/dashboard/login", request.url));
   }
-  let tokenCookie = request.cookies.get("admin-token");
-  console.log(tokenCookie);
-  if (typeof tokenCookie?.value == `string`) {
-    const { payload } = await jwtVerify(
-      tokenCookie.value,
-      new TextEncoder().encode(JWT_SECRET_ADMIN)
-    );
-    if (!payload) {
-      return NextResponse.redirect(new URL("/dashboard/login", request.url));
-    }
-  }
+  // let tokenCookie = request.cookies.get("admin-token");
+  // console.log(tokenCookie);
+  // if (typeof tokenCookie?.value == `string`) {
+  //   const { payload } = await jwtVerify(
+  //     tokenCookie.value,
+  //     new TextEncoder().encode(JWT_SECRET_ADMIN)
+  //   );
+  //   if (!payload) {
+  //     return NextResponse.redirect(new URL("/dashboard/login", request.url));
+  //   }
+  // }
 
   return;
 }
