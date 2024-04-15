71983d1eec90edc89d68389c234b3ae71f4936e0
mrsawy <mrsawy@gmail.com>
2024-04-15 03:50:37 +0200

    login form style

--- app/(dashboard)/dashboard/login/components/LoginCard.tsx
+++ app/(dashboard)/dashboard/login/components/LoginCard.tsx
@@ -1,16 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { DarkModeSwitch } from "@/components/navbar/darkmodeswitch";
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
-import { Input as LightInput } from "@/components/ui/input-light";
-import { adminLogin } from "@/actions/admin";
+// import { Button } from "@/components/ui/button";
+// import { DarkModeSwitch } from "@/components/navbar/darkmodeswitch";
+// import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+// import { Input as LightInput } from "@/components/ui/input-light";
+// import { adminLogin } from "@/actions/admin";
 import { cookies } from "next/headers";
 // import cookies from 'next-cookies';
 
 import { jwtVerify } from "jose";
-import prisma from "@/prisma/db";
+// import prisma from "@/prisma/db";
 import { redirect } from "next/navigation";
-
-import LoginForm from "./LoginForm";
+import Card from "./Card";
 
 // const JWT_SECRET_ADMIN: string = process.env.JWT_SECRET_ADMIN as string;
 // export const dynamic = 'force-no-store'
@@ -29,34 +28,10 @@ async function LoginCard() {
     } catch (error) {
       console.log(error);
       // return redirect(`/dashboard`);
-      return (
-        <Card className="py-4 min-w-[400px] ">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <div className="flex items-between justify-between w-full ">
-              <h2 className="text-lg font-semibold">Sign in to your account .</h2>
-              <DarkModeSwitch />
-            </div>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <LoginForm />
-          </CardBody>
-        </Card>
-      );
+      return <Card />;
     }
     return redirect(`/dashboard`);
   }
-  return (
-    <Card className="py-4 min-w-[400px] ">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <div className="flex items-between justify-between w-full ">
-          <h2 className="text-lg font-semibold">Sign in to your account .</h2>
-          <DarkModeSwitch />
-        </div>
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
-        <LoginForm />
-      </CardBody>
-    </Card>
-  );
+  return <Card />;
 }
 export default LoginCard;
