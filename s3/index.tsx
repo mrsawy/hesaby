d8b22a61317d1fb09513a0d9e8cc592323705e13
mrsawy <mrsawy@gmail.com>
2024-04-22 23:15:08 +0200

    s3 storage

--- s3/index.tsx
+++ s3/index.tsx
@@ -3,7 +3,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   region: "me-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
